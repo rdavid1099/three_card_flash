93bb25fc9bb1251ca49bc2d3d8bb6fed90e6f3ce
Ryan Workman <rdavid1099@users.noreply.github.com>
2023-07-12 10:37:33 -0600

    Add @ember-eui/core

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,13 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    svgJar: {
+      sourceDirs: [
+        'public/assets',
+        './node_modules/@ember-eui/core/public',
+        './node_modules/@ember-eui/core/vendor/icon',
+      ],
+    },
   });
 
   return app.toTree();
